236c642ed85e38938b89f541a98ff73a344d20c2
Danila Lebedev <daniilvlebedev@gmail.com>
2021-11-06 17:04:59 +0300

    Fixed comments and made getById a generics realization

--- src/main/java/Contracts/Contract.java
+++ src/main/java/Contracts/Contract.java
@@ -11,7 +11,7 @@ public abstract class Contract {
      * @param dateStart contracts starting date
      * @param dateEnd contracts ending date
      * @param number contracts number
-     * @param person - person, who has signed the contract
+     * @param person person, who has signed the contract
      */
     private int id;
     private LocalDate dateStart;
